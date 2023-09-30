a4537e1dd0d1529868d17770ede7ff3e8020bcbb
Andres Calizaya <andres.calizaya@naranjax.com>
2023-09-30 19:42:48 -0300

    fix lint

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/kitty/services/KittyMockService.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/kitty/services/KittyMockService.kt
@@ -8,31 +8,31 @@ import javax.inject.Singleton
 
 @Singleton
 class KittyMockService
-    @Inject
-    constructor() : KittyGetter {
-        override suspend fun getKitty(): Flow<Kitty> {
-            return flow {
-                emit(
-                    Kitty(
-                        id = "VEINTE MILLONES 四百",
-                        url = "https://img.freepik.com/foto-gratis/cerrar-lindo-gato-interior_23-2148882585.jpg",
-                        width = 100,
-                        height = 100,
-                    ),
-                )
-            }
-        }
-
-        override suspend fun getKittyById(id: Int): Flow<Kitty> {
-            return flow {
-                emit(
-                    Kitty(
-                        id = "$id",
-                        url = "https://img.freepik.com/foto-gratis/cerrar-lindo-gato-interior_23-2148882585.jpg",
-                        width = 100,
-                        height = 100,
-                    ),
-                )
-            }
+@Inject
+constructor() : KittyGetter {
+    override suspend fun getKitty(): Flow<Kitty> {
+        return flow {
+            emit(
+                Kitty(
+                    id = "VEINTE MILLONES 四百",
+                    url = "https://img.freepik.com/foto-gratis/cerrar-lindo-gato-interior_23-2148882585.jpg",
+                    width = 100,
+                    height = 100,
+                ),
+            )
         }
     }
+
+    override suspend fun getKittyById(id: Int): Flow<Kitty> {
+        return flow {
+            emit(
+                Kitty(
+                    id = "$id",
+                    url = "https://img.freepik.com/foto-gratis/cerrar-lindo-gato-interior_23-2148882585.jpg",
+                    width = 100,
+                    height = 100,
+                ),
+            )
+        }
+    }
+}
