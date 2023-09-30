0f3572130bb12e06cf3489ca493252cd52f7479e
Andres Calizaya <andres.calizaya@naranjax.com>
2023-09-30 19:35:12 -0300

    fix lint comments

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/kitty/network/KittyHTTPRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/kitty/network/KittyHTTPRepository.kt
@@ -5,11 +5,11 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class KittyHTTPRepository
-    @Inject
-    constructor(private val api: KittyAPI) : KittyNetworkRepository {
-        override suspend fun getRandomKitty(): Flow<KittyAPIModel> {
-            return flow {
-                emit(api.getKitties()[0])
-            }
+@Inject
+constructor(private val api: KittyAPI) : KittyNetworkRepository {
+    override suspend fun getRandomKitty(): Flow<KittyAPIModel> {
+        return flow {
+            emit(api.getKitties()[0])
         }
     }
+}
--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/kitty/repository/KittyDefaultRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/kitty/repository/KittyDefaultRepository.kt
@@ -7,9 +7,9 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class KittyDefaultRepository
-    @Inject
-    constructor(val networkRepository: KittyNetworkRepository) : KittyRepository {
-        override suspend fun getKitty(): Flow<Kitty> {
-            return this.networkRepository.getRandomKitty().map { it.toKitty() }
-        }
+@Inject
+constructor(val networkRepository: KittyNetworkRepository) : KittyRepository {
+    override suspend fun getKitty(): Flow<Kitty> {
+        return this.networkRepository.getRandomKitty().map { it.toKitty() }
     }
+}
